9fe3fd03c6600388f5c4afebdf34e8b1eabace39
Szabolcs Dombi <cprogrammer1994@gmail.com>
2023-07-20 21:20:45 +0300

    improved responsiveness

--- src/home.tsx
+++ src/home.tsx
@@ -23,7 +23,13 @@ const Post = ({ thumbnail, title, description, href }) => {
       }}
     >
       <div
-        style={{ flexShrink: 0 }}
+        style={{
+          flexShrink: 0,
+          height: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
       >
         {thumbnail}
       </div>
@@ -33,6 +39,7 @@ const Post = ({ thumbnail, title, description, href }) => {
           flexDirection: 'column',
           height: '200px',
           overflow: 'hidden',
+          paddingRight: '10px',
           flexGrow: 1,
         }}
       >
@@ -58,6 +65,8 @@ const Post = ({ thumbnail, title, description, href }) => {
   );
 };
 
+const thumbnailSize = 'min(30vw, 200px)';
+
 export const Home = () => {
   return (
     <>
@@ -65,6 +74,7 @@ export const Home = () => {
       <div
         style={{
           maxWidth: '725px',
+          padding: '0 20px',
           margin: 'auto',
         }}
       >
@@ -85,103 +95,103 @@ export const Home = () => {
       >
         <Post
           href="https://szabolcsdombi.com/otto-simulator/"
-          thumbnail={<OttoThumbnail size={200} resolution={600} />}
+          thumbnail={<OttoThumbnail size={thumbnailSize} resolution={400} />}
           title="Otto Simulator"
           description="This project brings Otto to your browser. Even better, it allows you to simulate Otto within Python. The simulation implements the Gym Interface."
         />
         <Post
           href="https://github.com/szabolcsdombi/blender-docker-cli"
-          thumbnail={<BlenderThumbnail size={200} />}
+          thumbnail={<BlenderThumbnail size={thumbnailSize} />}
           title="Blender in Docker"
           description="Exporting assets as a build step and more."
         />
         <Post
           href="https://github.com/szabolcsdombi/zengl/discussions/37"
-          thumbnail={<PythonThumbnail size={200} />}
+          thumbnail={<PythonThumbnail size={thumbnailSize} />}
           title="ZenGL and ModernGL"
           description="Why did I built two OpenGL Rendering libraries for Python?"
         />
         <Post
           href="https://szabolcsdombi.github.io/web-gym/"
-          thumbnail={<PythonThumbnail size={200} />}
+          thumbnail={<PythonThumbnail size={thumbnailSize} />}
           title="Gym in the Browser"
           description="Gymnasium is a maintained fork of OpenAI's Gym library. This experiment intends to bring it to the web for demo purposes."
         />
         <Post
           href="https://github.com/szabolcsdombi/zengl"
-          thumbnail={<ZenglThumbnail size={200} resolution={600} />}
+          thumbnail={<ZenglThumbnail size={thumbnailSize} resolution={400} />}
           title="ZenGL"
           description="Self-Contained OpenGL Rendering Pipelines for Python."
         />
         <Post
           href="https://github.com/moderngl/moderngl"
-          thumbnail={<ModernglThumbnail size={200} resolution={600} />}
+          thumbnail={<ModernglThumbnail size={thumbnailSize} resolution={400} />}
           title="ModernGL"
           description="ModernGL is a Python wrapper over OpenGL that simplifies the creation of simple graphics applications like scientific simulations, games or user interfaces."
         />
         <Post
           href="https://github.com/szabolcsdombi/python-rigging"
-          thumbnail={<PythonThumbnail size={200} />}
+          thumbnail={<PythonThumbnail size={thumbnailSize} />}
           title="Python Rigging"
           description="Python extension for rigging."
         />
         <Post
           href="https://github.com/szabolcsdombi/python-terrain"
-          thumbnail={<PythonThumbnail size={200} />}
+          thumbnail={<PythonThumbnail size={thumbnailSize} />}
           title="Python Terrain"
           description="Python extension for terrain manipulation."
         />
         <Post
           href="https://github.com/szabolcsdombi/python-mesh-join"
-          thumbnail={<PythonThumbnail size={200} />}
+          thumbnail={<PythonThumbnail size={thumbnailSize} />}
           title="Python Mesh Join"
           description="Python extension for joining indexed meshes."
         />
         <Post
           href="https://github.com/szabolcsdombi/python-capsule-shape"
-          thumbnail={<PythonThumbnail size={200} />}
+          thumbnail={<PythonThumbnail size={thumbnailSize} />}
           title="Python Capsule Shape"
           description="Python extension for creating a renderable capsule shape."
         />
         <Post
           href="https://github.com/szabolcsdombi/python-depth-sort"
-          thumbnail={<PythonThumbnail size={200} />}
+          thumbnail={<PythonThumbnail size={thumbnailSize} />}
           title="Python Depth Sort"
           description="Python extension for sorting the triangles of a mesh."
         />
         <Post
           href="https://github.com/szabolcsdombi/python-bezier"
-          thumbnail={<PythonThumbnail size={200} />}
+          thumbnail={<PythonThumbnail size={thumbnailSize} />}
           title="Python Bezier"
           description="Python extension for generating cubic bezier curves."
         />
         <Post
           href="https://github.com/szabolcsdombi/python-camera"
-          thumbnail={<PythonThumbnail size={200} />}
+          thumbnail={<PythonThumbnail size={thumbnailSize} />}
           title="Python Camera"
           description="Python extension implementing a simple camera for 3D projects."
         />
         <Post
           href="https://github.com/szabolcsdombi/python-mesh-raycast"
-          thumbnail={<PythonThumbnail size={200} />}
+          thumbnail={<PythonThumbnail size={thumbnailSize} />}
           title="Python Mesh Raycast"
           description="Python extension for calculating ray and triangle intersections."
         />
         <Post
           href="https://github.com/szabolcsdombi/python-mesh-normals"
-          thumbnail={<PythonThumbnail size={200} />}
+          thumbnail={<PythonThumbnail size={thumbnailSize} />}
           title="Python Mesh Normals"
           description="Python extension for generating mesh normals from vertex data."
         />
         <Post
           href="https://github.com/szabolcsdombi/python-quickhull"
-          thumbnail={<PythonThumbnail size={200} />}
+          thumbnail={<PythonThumbnail size={thumbnailSize} />}
           title="Python Quickhull"
           description="Python extension for building convex hulls from points."
         />
         <Post
           href="https://github.com/szabolcsdombi/python-compute-shader"
-          thumbnail={<PythonThumbnail size={200} />}
+          thumbnail={<PythonThumbnail size={thumbnailSize} />}
           title="Python Compute Shapder"
           description="Python extension interfacing the OpenGL Compute Shaders and Storage Buffers."
         />
